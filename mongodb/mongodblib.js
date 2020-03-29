7e7a2a46b282e289e2f77a3f6ff0b7b49729a903
CarlosOrduz <47404630+CarlosOrduz@users.noreply.github.com>
2020-03-29 13:54:41 -0500

    crud user

--- mongodb/mongodblib.js
+++ mongodb/mongodblib.js
@@ -3,7 +3,7 @@ const assert = require('assert');
 
 const url = 'mongodb+srv://usuario1:usuario1@petsociety-jo7y6.mongodb.net/test?retryWrites=true&w=majority';
 
-const dbName = 'PetSociety';
+const dbName = 'Proyecto3';
 
 const client = new MongoClient(url, { useUnifiedTopology: true });
 
@@ -15,8 +15,52 @@ const getDatabase = (callback) => {
         callback(db, client);
     });
 }
+const getAllUsers = function (db, callback) {
+    const collection = db.collection('users');
+    collection.find({}).toArray(function (err, docs) {
+        assert.equal(err, null);
+        callback(docs);
+    });
+}
 
+const getUserById = function (idU, db, callback) {
 
+    const collection = db.collection('users');
+    collection.findOne({ id: parseInt(idU) },function(err,user){
+        assert.equal(err, null);
+        callback(user);
+    });
+}
+const createUser = function (user, db, callback) {
+    const collection = db.collection('users');
+    collection.insertOne(user).then(callback(user));
+}
+const deleteUser = function (idU, db, callback) {
+    const collection = db.collection('users');
+    collection.deleteOne({ id: parseInt(idU) }).then(callback("User deleted"));
+}
+const updateUser = function (idU,user, db, callback) {
+    const collection = db.collection('users');
+ 
+    console.log(user);
+    collection.updateOne({ "id": parseInt(idU) },
+    {
+        $set: { "name": user.name,
+                "username":user.username,
+                "password":user.password,
+                "email":user.email,
+                "age": user.age,
+                "reviews":user.reviews
+    },
+        $currentDate: { lastModified: true }
+    }).then(callback(user));
+}
 exports.getDatabase = getDatabase;
+exports.getAllUsers=getAllUsers;
+exports.getUserById=getUserById;
+exports.createUser=createUser;
+exports.deleteUser=deleteUser;
+exports.updateUser=updateUser;
+
 
 
--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,41 @@
 var express = require('express');
 var router = express.Router();
-
+const Mongolib = require("../mongodb/mongodblib");
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+  Mongolib.getDatabase(db=>{
+    Mongolib.getAllUsers(db,docs=>{
+        res.send(docs);
+    })
+})
 });
-
+router.get('/:userId', function(req, res, next) {
+   
+  Mongolib.getDatabase(db=>{
+      Mongolib.getUserById(req.params.userId,db,docs=>{
+          res.send(docs);
+      })
+  })
+});
+router.post('/', function(req, res, next) {
+  Mongolib.getDatabase(db=>{
+      Mongolib.createUser(req.body,db,docs=>{
+          res.send(docs);
+      })
+  })
+});
+router.delete('/:userId', function(req,res,next){
+  Mongolib.getDatabase(db=>{
+      Mongolib.deleteUser(req.params.userId,db,docs=>{
+          res.send(docs);
+      })
+  })
+});
+router.put('/:userId', function(req,res,next){
+  Mongolib.getDatabase(db=>{
+      Mongolib.updateUser(req.params.userId,req.body[0],db,docs=>{
+          res.send(docs);
+      })
+  })
+});
 module.exports = router;
