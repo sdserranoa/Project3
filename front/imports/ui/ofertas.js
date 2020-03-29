6614a4b4c268928e833a52142a7f2a2eb97727e2
Louis Gualtero <lf.gualtero@uniandes.edu.co>
2020-03-29 16:37:28 -0500

    intentandoConectarDB

--- front/imports/ui/ofertas.js
+++ front/imports/ui/ofertas.js
@@ -1,5 +1,6 @@
 import { Template } from 'meteor/templating';
 import './ofertas.html';
+import { getAllOfertas } from '../../../mongodb/mongodblib.js';
 
 Template.ofertas.helpers({
   ofertas: [
@@ -7,5 +8,8 @@ Template.ofertas.helpers({
     { title: 'This is a great deal 2', description: 'buy 2 KrustyCrab get 1 free' },
     { title: 'This is a great deal 3', description: 'buy 3 KrustyCrab get 1 free' },
   ],
+  ofertasCollection: () => {
+    return getAllOfertas;
+  }
 });
 
