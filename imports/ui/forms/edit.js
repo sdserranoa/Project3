63c4e329d0ad40c5dae3b7716ed18dd15cc28ae0
CarlosOrduz <47404630+CarlosOrduz@users.noreply.github.com>
2020-03-30 21:18:58 -0500

    update perfil de usuario

--- imports/ui/forms/edit.js
+++ imports/ui/forms/edit.js
@@ -1,8 +1,31 @@
 import './edit.html';
 Template.perfil.helpers({
     
-    user: () => {
-        console.log(Meteor.user())
-        return Meteor.user();
+    username: () => {
+        
+        return Meteor.user().username;
+    },
+    userEmail: () => {
+       
+        return Meteor.user().emails[0].address;
+    }
+});
+
+Template.perfil.events({
+    'submit form': (event) =>{ 
+        event.preventDefault();
+        var nameVar = event.target.username.value;
+        console.log(event.target.username.value)
+        var emailVar = event.target.email.value;
+        console.log("Form submitted.");
+    
+
+
+     Meteor.users.update({_id: Meteor.user()._id}, {$set: {"username": nameVar,"emails[0].address":emailVar}})
+    
+        FlowRouter.redirect("/");
+    } ,
+    'reset form': (event) =>{ 
+        FlowRouter.redirect("/");
     }
 });
--- server/main.js
+++ server/main.js
@@ -2,5 +2,10 @@ import { Meteor } from 'meteor/meteor';
 import '../imports/api/ofertas.js';
 
 Meteor.startup(() => {
-  // code to run on server at startup
+  Meteor.users.allow({
+    update: function (userId, doc) {
+        console.log('UPDATE USER');
+        return true; 
+    }
+});
 });
