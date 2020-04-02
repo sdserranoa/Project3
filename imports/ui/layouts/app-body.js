eb78027d8fbbebfa010eef7426c09fc389004363
Louis Gualtero <lf.gualtero@uniandes.edu.co>
2020-04-02 16:14:07 -0500

    OfertasEstilizadas

--- imports/ui/layouts/app-body.js
+++ imports/ui/layouts/app-body.js
@@ -1,14 +1,7 @@
 
 import { Meteor } from 'meteor/meteor';
 import { ReactiveVar } from 'meteor/reactive-var';
-import { ReactiveDict } from 'meteor/reactive-dict';
-
-import { ActiveRoute } from 'meteor/zimme:active-route';
-import { FlowRouter } from 'meteor/kadira:flow-router';
-//import { TAPi18n } from 'meteor/tap:i18n';
-//import { T9n } from 'meteor/softwarerero:accounts-t9n';
 import { _ } from 'meteor/underscore';
-import { $ } from 'meteor/jquery';
 
 //import { Ofertas } from '../../api/lists/Ofertas.js';
 //import { insert } from '../../api/lists/methods.js';
